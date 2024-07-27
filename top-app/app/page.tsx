f540a67d642628eedf7706da7eff989ea7a3ab04
yarik.mik <yarik.mik@gmail.com>
2024-07-27 17:21:54 +0700

    Htag component

--- top-app/app/page.tsx
+++ top-app/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Htag } from '@/components';
 
 export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
-      Главная страница
+      {/* Главная страница */}
+      <Htag tag='h1'>Текст</Htag>
     </main>
   );
 }
