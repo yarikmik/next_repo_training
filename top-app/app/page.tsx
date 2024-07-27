ff0fcfe567ecff1661185f959a9f7e4d5ed98e3c
yarik.mik <yarik.mik@gmail.com>
2024-07-27 18:49:59 +0700

    svgr

--- top-app/app/page.tsx
+++ top-app/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button, Htag } from '@/components';
+import Logo from '../public/vercel.svg';
 
 export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
       {/* Главная страница */}
+      <Logo/>
       <Htag tag='h1'>Текст</Htag>
       <Button appearance='primary'>primary</Button>
       <Button appearance='ghost'>ghost</Button>
---
+++ top-app/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import("next").NextConfig} */
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
