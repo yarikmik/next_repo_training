54b2a7d1c53d9b54763f71f101f9b3cf9cfd9cfc
yarik.mik <yarik.mik@gmail.com>
2024-07-27 18:13:20 +0700

    add Button component

--- top-app/app/page.tsx
+++ top-app/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Htag } from '@/components';
+import { Button, Htag } from '@/components';
 
 export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
       {/* Главная страница */}
       <Htag tag='h1'>Текст</Htag>
+      <Button appearance='primary'>primary</Button>
+      <Button appearance='ghost'>ghost</Button>
     </main>
   );
 }
