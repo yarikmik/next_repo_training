57b13c1b4b64abf14ddd840963bbb8d6708739c5
yarik.mik <yarik.mik@gmail.com>
2024-08-02 20:02:29 +0700

    configs

--- top-app/next.config.js
+++ top-app/next.config.js
@@ -1,11 +1,33 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-};
+// module.exports = {
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /\.svg$/i,
+//       use: ['@svgr/webpack'],
+//     });
+//     return config;
+//   },
+// };
 
+module.exports = {
+	images: {
+		domains: ['courses-top.ru']
+	},
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+      issuer: /\.[jt]sx&$/,
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{ removeViewBox: false }],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
+
+		return config;
+	},
+};
