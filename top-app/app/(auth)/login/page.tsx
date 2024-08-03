bc177940c7ee1af80a63c84487d0762cbcc704e8
yarik.mik <yarik.mik@gmail.com>
2024-08-03 20:00:05 +0700

    группировка layouts

---
+++ top-app/app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Логин",
+};
+
+
+export default function Login(): JSX.Element {
+  return (
+    <div>
+      Логин
+    </div>
+  );
+}
