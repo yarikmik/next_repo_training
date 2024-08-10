9853b1a2976bf42332710ec57b1adbef621148ee
yarik.mik <yarik.mik@gmail.com>
2024-08-10 17:43:05 +0700

    ревалидация страниц

--- top-app/api/menu.tsx
+++ top-app/api/menu.tsx
@@ -5,7 +5,9 @@ export async function getMenu(firstCategory: number): Promise<MenuItem[]> {
   const res = await fetch(API.topPage.find, {
     method: 'POST',
     body: JSON.stringify({firstCategory}),
-    headers: new Headers({'content-type': 'application/json'})
+    headers: new Headers({'content-type': 'application/json'}),
+    next: { revalidate: 10 } // ревалидация странички через 10 секунд
   });
+  // console.log('revalidating getMenu');
   return res.json();
 }
--- top-app/api/page.tsx
+++ top-app/api/page.tsx
@@ -2,7 +2,11 @@ import { API } from '../app/api';
 import { TopPageModel } from '../app/interfaces/page.interface';
 
 export async function getPage(alias: string): Promise<TopPageModel | null> {
-  const res = await fetch(API.topPage.byAlias + alias);
+  const res = await fetch(API.topPage.byAlias + alias, {
+    next: { revalidate: 10 } // ревалидация странички через 10 секунд
+  });
+  // console.log('revalidating getPage');
+
   if (!res.ok) {
 	return null;
   }
