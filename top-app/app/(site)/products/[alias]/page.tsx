759504d09fade5ab62e97efd575a6559c8dc2f31
yarik.mik <yarik.mik@gmail.com>
2024-08-10 17:28:11 +0700

    SSG generateStaticParams

--- top-app/app/(site)/products/[alias]/page.tsx
+++ top-app/app/(site)/products/[alias]/page.tsx
@@ -1,12 +1,16 @@
-import { getPage } from '@/app/api/page';
-import styles from "./page.module.css";
+import { getPage } from '@/app/api/get-page';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import { getMenu } from '@/app/api/menu';
 
 export const metadata: Metadata = {
   title: "Страница",
 };
 
+export async function generateStaticParams() {
+  const menu = await getMenu(0);
+  return menu.flatMap(item => item.pages.map(page=>({ alias: page.alias })));
+}
 
 export default async function PageProducts({ params }: { params: {alias: string } } ): Promise<JSX.Element> {
   const page = await getPage(params.alias);
