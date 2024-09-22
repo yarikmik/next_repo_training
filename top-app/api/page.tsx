397b9efd2c381e05bbf41b52ffd4e34f6c5a1bb2
yarik.mik <yarik.mik@gmail.com>
2024-09-22 22:45:11 +0700

    loading

--- top-app/api/page.tsx
+++ top-app/api/page.tsx
@@ -2,6 +2,8 @@ import { API } from '../app/api';
 import { TopPageModel } from '../app/interfaces/page.interface';
 
 export async function getPage(alias: string): Promise<TopPageModel | null> {
+  await new Promise((res)=> setTimeout(()=>{res('');}, 2000));
+
   const res = await fetch(API.topPage.byAlias + alias, {
     next: { revalidate: 10 } // ревалидация странички через 10 секунд
   });
