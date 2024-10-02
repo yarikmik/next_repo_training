5777c62d06a42b7510dca159a47b11eb9820baa7
yarik.mik <yarik.mik@gmail.com>
2024-10-02 15:54:31 +0700

    обработка ошибок

--- top-app/components/Textarea/Textarea.props.ts
+++ top-app/components/Textarea/Textarea.props.ts
@@ -1,5 +1,5 @@
 import { DetailedHTMLProps, TextareaHTMLAttributes } from 'react';
-
+import { FieldError } from 'react-hook-form';
 export interface TextareaProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
-
+	error?: FieldError;
 }
